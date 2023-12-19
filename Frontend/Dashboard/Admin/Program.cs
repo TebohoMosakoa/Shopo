9250e8e001afe148b343002c478f364e9c5c3b47
mosak <mosak@TEBOHO>
2023-12-19 22:28:23 +0200

    Admin site start up

---
+++ Frontend/Dashboard/Admin/Program.cs
@@ -0,0 +1,12 @@
+using Admin;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:8000/api/") });
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
